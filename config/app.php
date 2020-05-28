157cd13818ef10fdb00cd798a9dcbac65e8adb9f
artur33s <artur.kachatrian@mail.ru>
2020-05-29 00:59:38 +0300

    Laravel-Form-Builder package add

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel-Form-builder (Kristijan Husak)
+        Kris\LaravelFormBuilder\FormBuilderServiceProvider::class
+
     ],
 
     /*
@@ -226,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'FormBuilder' => Kris\LaravelFormBuilder\Facades\FormBuilder::class
 
     ],
 
